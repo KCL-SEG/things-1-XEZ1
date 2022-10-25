7d30f0295cc34eac47209e74022d1af93565e56d
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-25 01:06:53 +0100

    Extend thing model

--- things/models.py
+++ things/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(
-    max_length = 15
+    max_length = 30
+    unique = True
     )
 
     description = models.CharField(
-    max_length = 250
+    max_length = 120
+    blank = False
     )
 
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(
+    max_value = 100
+    )
