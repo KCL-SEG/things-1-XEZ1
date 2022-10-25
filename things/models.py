81311724bc625a098d1b2efbd33c8de2d57cc276
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-25 01:09:01 +0100

    Extend thing model

--- things/models.py
+++ things/models.py
@@ -4,13 +4,17 @@ class Thing(models.Model):
     name = models.CharField(
     max_length = 30
     unique = True
+    blank = False
     )
 
     description = models.CharField(
     max_length = 120
+    unique = False
     blank = False
     )
 
     quantity = models.IntegerField(
+    min_value = 0
     max_value = 100
+    unique = False
     )
