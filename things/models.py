9581fe00830266dcf01dbbfec28c16dfa58a6fb2
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-25 01:25:34 +0100

    Extend thing model

--- things/models.py
+++ things/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(
