609f343f1742333fbca8b5260b100b96a62e2d19
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-26 09:35:41 +0100

    Redifne Sign Up form
    - Add Sign Up form unit tests
    - Add password validation to Sign Up form

--- things/tests/test_user_model.py
+++ things/tests/test_user_model.py
@@ -1,112 +1,12 @@
+"""Unit tests of the user model."""
 from django.test import TestCase
-from .models import Thing, User, Post
+from things.models import User
 from django.core.exceptions import ValidationError
 
 
 
-class ThinigModelTest(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            '@johndoe',
-            first_name='John',
-            last_name='Doe',
-            email='johndoe@example.org',
-            password='Password123',
-            bio='wag1'
-        )
-        self.thing = Thing.objects.create(
-            name='non-materia',
-            description='object of type non-material',
-            quantity=1
-        )
-
-    def test_valid_thing(self):
-        self._assert_thing_is_valid()
-
-
-
-    def test_things_name_cannot_be_blank(self):
-        self.thing.name = ''
-        self._assert_thing_is_invalid()
-
-    def test_name_cannot_be_more_than_30_characters_long(self):
-        self.thing.name = 'x' * 31
-        self._assert_thing_is_invalid()
-
-    def test_thing_name_can_be_30_characters_long(self):
-        self.thing.name = 'x' * 30
-        self._assert_thing_is_valid()
-
-    def test_thingname_must_be_uniqe(self):
-        second_thing = self._create_second_thing()
-        self.thing.name = second_thing.name
-        self._assert_thing_is_invalid()
-
-    def test_thingname_may_contain_numbers(self):
-        self.thing.thingname =  '@j0hndoe2'
-        self._assert_thing_is_valid()
-
-
-
-    def test_things_description_can_be_blank(self):
-        self.thing.description = ''
-        self._assert_thing_is_valid()
-
-    def test_description_cannot_be_more_than_120_characters_long(self):
-        self.thing.description = 'x' * 121
-        self._assert_thing_is_invalid()
-
-    def test_thing_description_can_be_120_characters_long(self):
-        self.thing.description = 'x' * 120
-        self._assert_thing_is_valid()
-
-    def test_thingdescription_can_be_not_uniqe(self):
-        second_thing = self._create_second_thing()
-        self.thing.description = second_thing.description
-        self._assert_thing_is_valid()
-
-
-
-    def test_quantity_cannot_be_more_than_120_characters_long(self):
-        self.thing.quantity = 101
-        self._assert_thing_is_invalid()
-
-    def test_thing_quantity_can_be_0_characters_long(self):
-        self.thing.quantity = 0
-        self._assert_thing_is_valid()
-
-    def test_thingquantity_can_be_not_uniqe(self):
-        second_thing = self._create_second_thing()
-        self.thing.quantity = second_thing.quantity
-        self._assert_thing_is_valid()
-
-
-
-    def _create_second_thing(self):
-        thing = Thing.objects.create(
-            name='material',
-            description='materail object',
-            quantity=1
-        )
-        return thing
-
-    def _assert_thing_is_valid(self):
-        try:
-            self.thing.full_clean()
-        except (ValidationError):
-            self.fail('Test thing should be valid')
-
-    def _assert_thing_is_invalid(self):
-        with self.assertRaises(ValidationError):
-            self.thing.full_clean()
-
-
-
-
-
 class UserModelTest(TestCase):
-
+    """Unit tests of the user model."""
     def setUp(self):
         self.user = User.objects.create_user(
             '@johndoe',
@@ -116,7 +16,6 @@ class UserModelTest(TestCase):
             password='Password123',
             bio='wag1'
         )
-
 
 
     def test_valid_user(self):
@@ -270,43 +169,3 @@ class UserModelTest(TestCase):
     def _assert_user_is_invalid(self):
         with self.assertRaises(ValidationError):
             self.user.full_clean()
-
-
-
-
-class PostTest(TestCase):
-    def setUp(self):
-        super(TestCase, self).setUp()
-        self.user = User.objects.create_user(
-            '@johndoe',
-            first_name='John',
-            last_name='Doe',
-            email='johndoe@example.org',
-            password='Password123',
-            bio='The quick brown fox jumps over the lazy dog.'
-        )
-        self.post = Post(
-            author=self.user,
-            text="The quick brown fox jumps over the lazy dog."
-        )
-
-    def test_valid_message(self):
-        try:
-            self.post.full_clean()
-        except ValidationError:
-            self.fail("Test message should be valid")
-
-    def test_author_must_not_be_blank(self):
-        self.post.author = None
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_blank(self):
-        self.post.text = ''
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_overlong(self):
-        self.post.text = 'x' * 281
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
